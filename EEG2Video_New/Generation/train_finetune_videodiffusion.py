36c4614f3befa1e2a9ff1d6280e8e48c7b6ad8eb
sjtuwangjunke <144574920+sjtuwangjunke@users.noreply.github.com>
2025-09-09 14:12:46 +0800

    Update train_finetune_videodiffusion.py

--- EEG2Video_New/Generation/train_finetune_videodiffusion.py
+++ EEG2Video_New/Generation/train_finetune_videodiffusion.py
@@ -1,76 +1,379 @@
-from pipelines.pipeline_tuneeeg2video import TuneAVideoPipeline
-from models.unet import UNet3DConditionModel
-from transformers import CLIPTextModel, CLIPTokenizer
-from tuneavideo.util import save_videos_grid,ddim_inversion
-import torch
-from models.train_semantic_predictor import CLIP
-import numpy as np
-from einops import rearrange
-from sklearn import preprocessing
-import random
+import argparse
+import logging
+import inspect
 import math
-model = None
-torch.cuda.empty_cache()
-torch.cuda.ipc_collect()
 import os
-from diffusers.schedulers import (
-    DDPMScheduler,
-    DDIMScheduler,
-)
-torch.cuda.set_device(0)
-def seed_everything(seed=0, cudnn_deterministic=True):
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    if cudnn_deterministic:
-        torch.backends.cudnn.deterministic = True
+from typing import Dict, Optional, Tuple
+from omegaconf import OmegaConf
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+import diffusers
+import transformers
+from accelerate import Accelerator
+from accelerate.logging import get_logger
+from accelerate.utils import set_seed
+from diffusers import AutoencoderKL, DDPMScheduler, DDIMScheduler
+from diffusers.optimization import get_scheduler
+from diffusers.utils import check_min_version
+from diffusers.utils.import_utils import is_xformers_available
+from tqdm import tqdm
+from transformers import CLIPTextModel, CLIPTokenizer
+
+from models.unet import UNet3DConditionModel
+from tuneavideo.data.dataset import TuneMultiVideoDataset
+from pipelines.pipeline_tuneavideo import TuneAVideoPipeline
+from tuneavideo.util import save_videos_grid, ddim_inversion
+from einops import rearrange
+
+
+os.environ["PYTORCH_CUDA_ALLOC_conf"] = "max_split_size_mb:24"
+
+# Will error if the minimal version of diffusers is not installed. Remove at your own risks.
+# check_min_version("0.10.0.dev0")
+
+logger = get_logger(__name__, log_level="INFO")
+GT_label = np.array([[23, 22, 9, 6, 18,       14, 5, 36, 25, 19,      28, 35, 3, 16, 24,      40, 15, 27, 38, 33, 
+             34, 4, 39, 17, 1,       26, 20, 29, 13, 32,     37, 2, 11, 12, 30,      31, 8, 21, 7, 10, ],
+            [27, 33, 22, 28, 31,     12, 38, 4, 18, 17,      35, 39, 40, 5, 24,      32, 15, 13, 2, 16,
+ 	         34, 25, 19, 30, 23,     3, 8, 29, 7, 20,        11, 14, 37, 6, 21,      1, 10, 36, 26, 9, ],
+            [15, 36, 31, 1, 34,      3, 37, 12, 4, 5,        21, 24, 14, 16, 39,     20, 28, 29, 18, 32, 
+             2, 27, 8, 19, 13,       10, 30, 40, 17, 26,     11, 9, 33, 25, 35,      7, 38, 22, 23, 6,],
+            [16, 28, 23, 1, 39,      10, 35, 14, 19, 27,     37, 31, 5, 18, 11,      25, 29, 13, 20, 24, 
+            7, 34, 26, 4, 40 ,       12, 8, 22, 21, 30,      17, 2, 38, 9,  3 ,      36, 33, 6, 32, 15,],
+            [18, 29, 7, 35, 22  ,    19, 12, 36, 8, 15,      28, 1, 34, 23, 20 ,     13, 37, 9, 16, 30  ,  
+             2, 33, 27, 21, 14 ,     38, 10, 17, 31, 3,      24, 39, 11, 32, 4,      25, 40, 5, 26, 6 ,],
+            [29, 16, 1, 22, 34,      39, 24, 10, 8, 35,      27, 31, 23, 17, 2,      15, 25, 40, 3, 36, 
+             26, 6, 14, 37, 9,       12, 19, 30, 5, 28,      32, 4, 13, 18, 21,      20, 7, 11, 33, 38],
+            [38, 34, 40, 10, 28,     7, 1, 37, 22, 9,        16, 5, 12, 36, 20,      30, 6, 15, 35, 2,      
+             31, 26, 18, 24, 8,      3, 23, 19, 14, 13,      21, 4, 25, 11, 32,      17, 39, 29, 33, 27]
+            ])
+GT_label = GT_label - 1
+All_label = np.empty((0, 200))
+for block_id in range(7):
+    All_label = np.concatenate((All_label, GT_label[block_id].repeat(5).reshape(1, 200)))
+chosed_label = [i for i in range(40)]
+selected_indices = np.where(np.isin(All_label[0].flatten(), chosed_label))[0] 
+
+
+def main(
+    pretrained_model_path: str,
+    output_dir: str,
+    train_data: Dict,
+    validation_data: Dict,
+    validation_steps: int = 100,
+    trainable_modules: Tuple[str] = (
+        "attn1.to_q",
+        "attn2.to_q",
+        "attn_temp",
+    ),
+    train_batch_size: int = 1,
+    max_train_steps: int = 12000,
+    learning_rate: float = 3e-5,
+    scale_lr: bool = False,
+    lr_scheduler: str = "constant",
+    lr_warmup_steps: int = 0,
+    adam_beta1: float = 0.9,
+    adam_beta2: float = 0.999,
+    adam_weight_decay: float = 1e-2,
+    adam_epsilon: float = 1e-08,
+    max_grad_norm: float = 1.0,
+    gradient_accumulation_steps: int = 1,
+    gradient_checkpointing: bool = True,
+    checkpointing_steps: int = 500,
+    resume_from_checkpoint: Optional[str] = None,
+    mixed_precision: Optional[str] = "fp16",
+    use_8bit_adam: bool = False,
+    enable_xformers_memory_efficient_attention: bool = True,
+    seed: Optional[int] = None,
+):
+    *_, config = inspect.getargvalues(inspect.currentframe())
+
+    accelerator = Accelerator(
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        mixed_precision=mixed_precision,
+    )
+
+    # Make one log on every process with the configuration for debugging.
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+    logger.info(accelerator.state, main_process_only=False)
+    if accelerator.is_local_main_process:
+        transformers.utils.logging.set_verbosity_warning()
+        diffusers.utils.logging.set_verbosity_info()
     else:
-        ## needs to be False to use conv3D
-        print('Note: not using cudnn.deterministic')
-seed_everything(114514)
-#eeg = torch.load('EEG2Video/EEG2Video_New/Semantic/text_embeddings/block7.pt',map_location='cpu',weights_only=False)
-#eeg = rearrange(eeg, 'a b c -> a (b c)')  # [200, 77*768]
-eeg = torch.load('EEG2Video/EEG2Video_New/Semantic/sub1_session7_embeddings.pt',map_location='cpu',weights_only=False)
-negative = eeg.mean(dim=0)
+        transformers.utils.logging.set_verbosity_error()
+        diffusers.utils.logging.set_verbosity_error()
 
-pretrained_model_path = "CompVis/stable-diffusion-v1-4"
-my_model_path = "outputs/40_classes_200_epoch"
+    # If passed along, set the training seed now.
+    if seed is not None:
+        set_seed(seed)
 
-unet = UNet3DConditionModel.from_pretrained(my_model_path, subfolder='unet', torch_dtype=torch.float16).to('cuda')
-pipe = TuneAVideoPipeline.from_pretrained(pretrained_model_path ,unet=unet, torch_dtype=torch.float16).to("cuda")
-pipe.enable_xformers_memory_efficient_attention()
-pipe.enable_vae_slicing()
+    # Handle the output folder creation
+    if accelerator.is_main_process:
+        os.makedirs(output_dir, exist_ok=True)
+        os.makedirs(f"{output_dir}/samples", exist_ok=True)
+        os.makedirs(f"{output_dir}/inv_latents", exist_ok=True)
+        OmegaConf.save(config, os.path.join(output_dir, 'config.yaml'))
 
-latents = np.load('EEG2Video/EEG2Video_New/Seq2Seq/latent_out_block7_40_classes.npy')
-latents = torch.from_numpy(latents).half()
-latents = rearrange(latents, 'a b c d e -> a c b d e')
-latents = latents.to('cuda')
+    # Load scheduler, tokenizer and models.
+    noise_scheduler = DDPMScheduler.from_pretrained(pretrained_model_path, subfolder="scheduler")
+    tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_path, subfolder="tokenizer")
+    text_encoder = CLIPTextModel.from_pretrained(pretrained_model_path, subfolder="text_encoder")
+    vae = AutoencoderKL.from_pretrained(pretrained_model_path, subfolder="vae")
+    unet = UNet3DConditionModel.from_pretrained_2d(pretrained_model_path, subfolder="unet")
 
-latents_add_noise = torch.load('EEG2Video/EEG2Video_New/DANA/40_classes_latent_add_noise.pt')
-latents_add_noise = latents_add_noise.half()
-latents_add_noise = rearrange(latents_add_noise, 'a b c d e -> a c b d e')
-latents_add_noise = latents_add_noise.to('cuda')
-# Ablation, inference w/o Seq2Seq and w/o DANA
-woSeq2Seq = True
-woDANA = False
-for i in range(len(eeg)):
-    if woSeq2Seq:
-        video = pipe(model, eeg[i:i+1,...],negative_eeg=negative, latents=None, video_length=6, height=288, width=512, num_inference_steps=100, guidance_scale=12.5).videos
-        savename = f'40_Classes_woSeq2Seq/EEG2Video/'
-        import os
-        os.makedirs(savename, exist_ok=True)
-    elif woDANA:
-        video = pipe(model, eeg[i:i+1,...],negative_eeg=negative, latents=latents[i:i+1,...], video_length=6, height=288, width=512, num_inference_steps=100, guidance_scale=12.5).videos
-        savename = f'40_Classes_woDANA/EEG2Video/'
-        import os
-        os.makedirs(savename, exist_ok=True)
+    # Freeze vae and text_encoder
+    vae.requires_grad_(False)
+    text_encoder.requires_grad_(False)
+
+    unet.requires_grad_(False)
+    for name, module in unet.named_modules():
+        if name.endswith(tuple(trainable_modules)):
+            for params in module.parameters():
+                params.requires_grad = True
+
+    if enable_xformers_memory_efficient_attention:
+        if is_xformers_available():
+            unet.enable_xformers_memory_efficient_attention()
+        else:
+            raise ValueError("xformers is not available. Make sure it is installed correctly")
+
+    if gradient_checkpointing:
+        unet.enable_gradient_checkpointing()
+
+    if scale_lr:
+        learning_rate = (
+            learning_rate * gradient_accumulation_steps * train_batch_size * accelerator.num_processes
+        )
+
+    # Initialize the optimizer
+    if use_8bit_adam:
+        try:
+            import bitsandbytes as bnb
+        except ImportError:
+            raise ImportError(
+                "Please install bitsandbytes to use 8-bit Adam. You can do so by running `pip install bitsandbytes`"
+            )
+
+        optimizer_cls = bnb.optim.AdamW8bit
     else:
-        video = pipe(model, eeg[i:i+1,...],negative_eeg=negative, latents=latents_add_noise[i:i+1,...], video_length=6, height=288, width=512, num_inference_steps=100, guidance_scale=12.5).videos
-        savename = f'40_Classes_Fullmodel/EEG2Video/'
-        import os
-        os.makedirs(savename, exist_ok=True)
-    save_videos_grid(video, f"./{savename}/{i}.gif")
- 
+        optimizer_cls = torch.optim.AdamW
+
+    optimizer = optimizer_cls(
+        unet.parameters(),
+        lr=learning_rate,
+        betas=(adam_beta1, adam_beta2),
+        weight_decay=adam_weight_decay,
+        eps=adam_epsilon,
+    )
+
+    # Get the training dataset
+    train_dataset = TuneMultiVideoDataset(**train_data)
+    video_path = 'SEED-DV/Video_Mp4/Block1'
+    video_files = []
+    video_ind = selected_indices
+    for ind in video_ind:
+        file = video_path + '/' + str(ind+1) + '.mp4'
+        video_files.append(file)
+        
+    train_dataset.video_path = video_files
+
+    text_prompts = []
+    video_text = 'SEED-DV/Video/BLIP-caption/1st_10min.txt'
+
+    with open(video_text, 'r') as f:
+        for line in f:
+            text_prompts.append(line.strip())
+    selected_texts = [text_prompts[ind].strip() for ind in selected_indices]
+        
+    train_dataset.prompt = selected_texts
+    print("video_path_length:", len(train_dataset.video_path))
+    print("prompt_length:", len(train_dataset.prompt))
+    
+    # Preprocessing the dataset
+    train_dataset.prompt_ids = tokenizer(
+        list(train_dataset.prompt), max_length=tokenizer.model_max_length, padding="max_length", truncation=True, return_tensors="pt"
+    ).input_ids
+
+    # DataLoaders creation:
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=train_batch_size, shuffle=True
+    )
+
+    # Get the validation pipeline
+    validation_pipeline = TuneAVideoPipeline(
+        vae=vae, text_encoder=text_encoder, tokenizer=tokenizer, unet=unet,
+        scheduler=DDIMScheduler.from_pretrained(pretrained_model_path, subfolder="scheduler")
+    )
+    validation_pipeline.enable_vae_slicing()
+    ddim_inv_scheduler = DDIMScheduler.from_pretrained(pretrained_model_path, subfolder='scheduler')
+    ddim_inv_scheduler.set_timesteps(validation_data.num_inv_steps)
+    num_train_epochs = 200
+
+    # Scheduler
+    lr_scheduler = get_scheduler(
+        lr_scheduler,
+        optimizer=optimizer,
+        num_warmup_steps=lr_warmup_steps * gradient_accumulation_steps,
+        num_training_steps=num_train_epochs  * len(train_dataloader) * gradient_accumulation_steps,
+    )
+
+    # Prepare everything with our `accelerator`.
+    unet, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+        unet, optimizer, train_dataloader, lr_scheduler
+    )
+
+    # For mixed precision training we cast the text_encoder and vae weights to half-precision
+    # as these models are only used for inference, keeping weights in full precision is not required.
+    weight_dtype = torch.float32
+    if accelerator.mixed_precision == "fp16":
+        weight_dtype = torch.float16
+    elif accelerator.mixed_precision == "bf16":
+        weight_dtype = torch.bfloat16
+
+    # Move text_encode and vae to gpu and cast to weight_dtype
+    text_encoder.to(accelerator.device, dtype=weight_dtype)
+    vae.to(accelerator.device, dtype=weight_dtype)
+
+    # We need to recalculate our total training steps as the size of the training dataloader may have changed.
+    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / gradient_accumulation_steps)
+    # Afterwards we recalculate our number of training epochs
+    #num_train_epochs = math.ceil(max_train_steps / num_update_steps_per_epoch)
+    
+
+    # We need to initialize the trackers we use, and also store our configuration.
+    # The trackers initializes automatically on the main process.
+    if accelerator.is_main_process:
+        accelerator.init_trackers("text2video-fine-tune")
+
+    # Train!
+    total_batch_size = train_batch_size * accelerator.num_processes * gradient_accumulation_steps
+
+    logger.info("***** Running training *****")
+    logger.info(f"  Num examples = {len(train_dataset)}")
+    logger.info(f"  Num Epochs = {num_train_epochs}")
+    logger.info(f"  Instantaneous batch size per device = {train_batch_size}")
+    logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")
+    logger.info(f"  Gradient Accumulation steps = {gradient_accumulation_steps}")
+    global_step = 0
+    first_epoch = 1
+
+    # Only show the progress bar once on each machine.
+    for epoch in tqdm(range(first_epoch, num_train_epochs+1)):
+        unet.train()
+        train_loss = 0.0
+        for step, batch in enumerate(train_dataloader):
+            with accelerator.accumulate(unet):
+                # Convert videos to latent space
+                pixel_values = batch["pixel_values"].to(weight_dtype)
+                # print("pixel_values:", pixel_values.shape)
+                video_length = pixel_values.shape[1]
+                pixel_values = rearrange(pixel_values, "b f c h w -> (b f) c h w")
+                latents = vae.encode(pixel_values).latent_dist.sample()
+                latents = rearrange(latents, "(b f) c h w -> b c f h w", f=video_length)
+                # print("latents:", latents.shape)
+                latents = latents * 0.18215
+
+                # Sample noise that we'll add to the latents
+                noise = torch.randn_like(latents)
+                bsz = latents.shape[0]
+                # Sample a random timestep for each video
+                timesteps = torch.randint(0, noise_scheduler.num_train_timesteps, (bsz,), device=latents.device)
+                timesteps = timesteps.long()
+
+                # Add noise to the latents according to the noise magnitude at each timestep
+                # (this is the forward diffusion process)
+                noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
+
+                # Get the text embedding for conditioning
+                encoder_hidden_states = text_encoder(batch["prompt_ids"])[0]
+
+                # Get the target for loss depending on the prediction type
+                if noise_scheduler.prediction_type == "epsilon":
+                    target = noise
+                elif noise_scheduler.prediction_type == "v_prediction":
+                    target = noise_scheduler.get_velocity(latents, noise, timesteps)
+                else:
+                    raise ValueError(f"Unknown prediction type {noise_scheduler.prediction_type}")
+
+                # Predict the noise residual and compute loss
+                model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
+                loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
+
+                # Gather the losses across all processes for logging (if we use distributed training).
+                avg_loss = accelerator.gather(loss.repeat(train_batch_size)).mean()
+                train_loss += avg_loss.item() / gradient_accumulation_steps
+
+                # Backpropagate
+                accelerator.backward(loss)
+                if accelerator.sync_gradients:
+                    accelerator.clip_grad_norm_(unet.parameters(), max_grad_norm)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+
+            # Checks if the accelerator has performed an optimization step behind the scenes
+            if accelerator.sync_gradients:
+                #progress_bar.update(1)
+                global_step += 1
+                accelerator.log({"train_loss": train_loss}, step=global_step)
+                train_loss = 0.0
+
+            logs = {"step_loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
+            #progress_bar.set_postfix(**logs) 
+
+        if epoch % 50 == 0:
+            if accelerator.is_main_process:
+                samples = []
+                generator = torch.Generator(device=latents.device)
+                generator.manual_seed(seed)
+                for idx, prompt in enumerate(validation_data.prompts):
+                    sample = validation_pipeline(prompt, generator=generator, latents=None,
+                                                 **validation_data).videos
+
+                   
+                    save_videos_grid(sample, f"{output_dir}/samples/sample-{epoch}/{prompt}.gif")
+                    samples.append(sample)
+                samples = torch.concat(samples)
+                save_path = f"{output_dir}/samples/sample-{epoch}.gif"
+                save_videos_grid(samples, save_path)
+                logger.info(f"Saved samples to {save_path}")
+                
+
+            accelerator.wait_for_everyone()
+            if accelerator.is_main_process:
+                unet = accelerator.unwrap_model(unet)
+                pipeline = TuneAVideoPipeline.from_pretrained(
+                    pretrained_model_path,
+                    text_encoder=text_encoder,
+                    vae=vae,
+                    unet=unet,
+                )
+                pipeline.save_pretrained(output_dir)
+
+    # Create the pipeline using the trained modules and save it.
+    accelerator.wait_for_everyone()
+    if accelerator.is_main_process:
+        unet = accelerator.unwrap_model(unet)
+        pipeline = TuneAVideoPipeline.from_pretrained(
+            pretrained_model_path,
+            text_encoder=text_encoder,
+            vae=vae,
+            unet=unet,
+        )
+        pipeline.save_pretrained(output_dir)
+
+    accelerator.end_training()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default="./configs/all_40_video.yaml")
+    args = parser.parse_args()
+
+    main(**OmegaConf.load(args.config))
